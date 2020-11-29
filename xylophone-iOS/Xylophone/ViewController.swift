e3210bdb922312f8cab11b63bd591fca5be8343a
Dulio Denis <dulio.denis@yahoo.com>
2020-11-29 07:30:25 -0500

    Play Sound with AVFoundation

--- xylophone-iOS/Xylophone/ViewController.swift
+++ xylophone-iOS/Xylophone/ViewController.swift
@@ -7,17 +7,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("C Button Tapped")
+        playSound(resource: sender.currentTitle!)
     }
     
+    func playSound(resource: String) {
+            let url = Bundle.main.url(forResource: resource, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+                    
+        }
 }
 
